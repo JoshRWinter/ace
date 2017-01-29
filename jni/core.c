6a14fb12849fdd3274813a91b8d02a50f8ca1200
Josh Winter <winterjoshr@gmail.com>
2017-01-28 22:49:38 -0500

    now easier to shoot down missiles

--- jni/core.c
+++ jni/core.c
@@ -308,7 +308,7 @@ int core(struct state *state){
 		// check for bullets colliding with missiles
 		int stop=false;
 		for(struct missile *missile=state->missilelist,*prevmissile=NULL;missile!=NULL;){
-			if(collide(&bullet->base,&missile->base,-0.03f)){
+			if(collide(&bullet->base,&missile->base,-0.3f)){
 				if(!state->player.dead){
 					sprintf(msg,"+%d missile shot down",POINTS_MISSILE_SHOT_DOWN);
 					newmessage(state,msg);
